b94652aeb8328fc5d26c87aae59ebb23a06218a4
MichaelAbe <michael.k.abe@gmail.com>
2021-11-02 12:30:11 -0400

    hard coded users in the dropdown

--- src/Components/User.js
+++ src/Components/User.js
@@ -4,6 +4,20 @@ export default function User() {
     return (
         <div>
             <h1>User Component</h1>
+            <select name="userselect" id="userselect" className="userselect">
+            <option value="Ariel">Ariel</option>  
+            <option value="Genna">Genna</option>
+            <option value="Kealoha">Kealoha</option>
+            <option value="Kekoanui">Kekoanui</option>
+            <option value="Kevin">Kevin</option>
+            <option value="Lani">Lani</option>
+            <option value="Jon">Jon</option>
+            <option value="Preston">Preston</option>
+            <option value="Taylor">Taylor</option>
+            <option value="Kyle">Kyle</option>
+            <option value="Meredith">Meredith</option>
+            <option value="Mikey">Mikey</option>
+          </select>
         </div>
     )
 }
